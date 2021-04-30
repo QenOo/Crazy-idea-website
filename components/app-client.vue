e0e5d78628e95ced611df85a5565382468d75f4e
QeNo <moodiiy.98@gmail.com>
2021-04-30 23:44:22 +0200

    create component clients

---
+++ components/app-client.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="clients">
+
+        <h5 class="text-sec-head text-center">Clients we work for</h5>
+        
+        <v-sheet
+            class="mx-auto"
+            max-width="1200"
+        >
+            <v-slide-group
+                show-arrows
+                v-model="model"
+                >
+                <v-slide-item
+                    v-for="item in this.res"
+                    :key="item.id"
+                >
+                    <img class="imageClient" :src="$images(item.client_image, 'medium')" />
+                </v-slide-item>
+            </v-slide-group>
+        </v-sheet>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        model: null,
+        res: [],
+    }),
+    mounted() {
+            this.fetchSomething();
+    },
+    methods: {
+        async fetchSomething() {
+            let url = process.env.moduleUrl + 'client';
+            let image = process.env.imageUrl;
+            console.log(image)
+            const res = await this.$axios.$get(url);
+            this.res = res.payload.data;
+        }
+    }
+}
+</script>
+
+<style>
+.imageClient {
+    width: 150px;
+    height: 60px;
+    margin: 0 100px;
+}
+
+.text-sec-head {
+    font-size: 40px;
+    margin: 50px 0 50px 0;
+}
+</style>
