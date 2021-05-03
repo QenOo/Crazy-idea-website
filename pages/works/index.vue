c5348e53eb47bddbb80435d5bbbc1983f0928b7d
QeNo <moodiiy.98@gmail.com>
2021-05-03 14:17:09 +0200

    test online

--- pages/works/index.vue
+++ pages/works/index.vue
@@ -1,20 +1,21 @@
 <template>
     <div class="works">
-                <!--start head-section-page -->
-                <div class="head-section-page">
-                    <v-container>
-                        <v-row class="d-flex align-center">
-                            <v-col lg="6" md="6" sm="12">
-                                <img src="/pagesImages/Works.svg">
-                            </v-col>
-                            <v-col lg="6" md="6" sm="12">
-                                <h2>{{$t('works')}}</h2>
-                            </v-col>
-                        </v-row>
-                    </v-container>
+        <!--start head-section-page -->
+        <div class="head-section-page">
 
-                </div>
-                <!--end head-section-page -->
+            <v-container>
+                <v-row class="d-flex align-center">
+                    <v-col cols="12" lg="6" md="6" sm="6" xm="12">
+                        <img src="/pagesImages/services.svg">
+                    </v-col>
+                    <v-col cols="12" lg="6" md="6" sm="6" xm="12">
+                        <h2>{{$t('works')}}</h2>
+                    </v-col>
+                </v-row>
+            </v-container>
+            
+        </div>
+        <!--end head-section-page -->
     </div>
 </template>
 
