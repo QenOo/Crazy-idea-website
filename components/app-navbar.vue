7bdd203f6715ca19c62e13247ed708df152b0469
QeNo <moodiiy.98@gmail.com>
2021-05-07 16:41:24 +0200

    add logo white and black

--- components/app-navbar.vue
+++ components/app-navbar.vue
@@ -1,36 +1,38 @@
 <template>
     <div class="my-navbar">
+            <!-- Nav bar -->
+            <v-app-bar height="100px" app>
+        <v-container class="pa-0 fill-height">
 
-        <!-- Nav bar -->
-        <v-app-bar height="100px" app>
 
+                <v-toolbar-title>
 
-            <v-toolbar-title>
-                <v-img  src="/logo.svg" width="80px" ></v-img>
-            </v-toolbar-title>
+                    <v-img v-if=" this.$vuetify.theme.dark == false" src="/logo.svg" width="70px" ></v-img>
+                    <v-img v-else src="/logow.svg" width="60px" ></v-img>
+                </v-toolbar-title>
 
-            <v-spacer></v-spacer>
-            
-            <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = true"></v-app-bar-nav-icon>
-            
-            <v-toolbar-items class="hidden-sm-and-down">
-                    
-                <v-btn text v-for="item in menuItems" :key="item.index" :to="item.path">
-                    <v-icon small > {{ item.icon }} </v-icon>
-                    <span> {{$t(`${item.title}`)}} </span>
-                </v-btn>
-
+                <v-spacer></v-spacer>
                 
-            </v-toolbar-items>
+                <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = true"></v-app-bar-nav-icon>
+                
+                <v-toolbar-items class="hidden-sm-and-down">
+                        
+                    <v-btn text v-for="item in menuItems" :key="item.index" :to="item.path">
+                        <v-icon small > {{ item.icon }} </v-icon>
+                        <span> {{$t(`${item.title}`)}} </span>
+                    </v-btn>
+
+                    
+                </v-toolbar-items>
 
 
 
-        </v-app-bar>
-
+        </v-container>
+            </v-app-bar>
         <v-navigation-drawer v-model="drawer" absolute temporary>
 
             <v-list nav dense>
-                <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4" >
+                <v-list-item-group v-model="group" active-class="textinfo--text text--accent-4" >
 
                     <v-list-item v-for="item in menuItems" :key="item.index" :to="item.path">
                         <v-list-item-icon>
@@ -81,4 +83,32 @@ export default {
     left: 0;
     bottom: 0;
 }
+
+.v-btn--active:before,
+.v-btn:hover {
+    background-color: transparent;
+    border-bottom: 4px solid transparent;
+    opacity: 1;
+}
+
+.v-btn:hover {
+    background-color: #006eff77;
+}
+
+.v-btn--active:before {
+    border-bottom: 4px solid #428EF3;
+}
+.v-btn {
+    border-bottom: 4px solid transparent;
+}
+
+a.v-btn--active span {
+    color: #428EF3;
+}
+
+.theme--dark.v-btn--active:hover::before, .theme--dark.v-btn--active::before,
+.theme--light.v-btn--active:hover::before, .theme--light.v-btn--active::before {
+    opacity: 1;
+}
+
 </style>
